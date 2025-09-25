0af46df0fe0f041536117f05b73526496a19c3a6
Vaibhav <rishu2003b@gmail.com>
2025-09-25 22:27:36 +0530

    search result display issue resolved

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FlightSearchPage from "./FlightSearchPage";
 import Flightdetails from "./Flightdetails";
+import Bookingstatus from "./Bookingstatus";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Route path="/" element={<FlightSearchPage />} />
         <Route path="/login" element={<h2>Login Page</h2>} />
         <Route path="/signup" element={<h2>Signup Page</h2>} />
-        <Route path = "/booking-status" element={<h2>bookingpage</h2>} />
+        <Route path = "/booking-status" element={<h2><Bookingstatus /></h2>} />
         <Route path="/flightdetails" element={<Flightdetails /> } /> 
         {/* <Flightdetails /> */}
       </Routes>
